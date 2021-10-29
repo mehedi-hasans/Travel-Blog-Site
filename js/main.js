afdaed606cb47226ff59aa8312fdc7b60e226948
mehedi115 <shihab3676@gmail.com>
2021-10-29 02:02:03 -0400

    Full Project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.menu').slicknav();
+
+$('.slider-full').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.slide-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
